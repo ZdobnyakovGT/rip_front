49b94ef27ad9f550e294b1b15ddf2cd87c33ebc0
Fedor <F.zdobnyakov@gmail.com>
2024-11-17 19:12:54 +0300

    n

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from "react-router-dom";
+// import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename="/">
         <App />
-    </BrowserRouter>
 )
 
 
